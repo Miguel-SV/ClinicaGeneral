7721cf1aab2a589d06c7dc1ab5d697d47cedd34e
Miguel Amaya <Miguel Amaya@DESKTOP-QVD2PQF>
2017-10-10 10:58:53 -0600

    Primer avanze de diseño

--- menu.php
+++ menu.php
@@ -1,7 +1,7 @@
 
 
 <nav>
-    <div class="nav-wrapper blue">
+    <div class="nav-wrapper light-blue darken-4">
         <div class="container">
             <a href="#" class="brand-logo">Clinica Medica</a>
             <ul id="nav-mobile" class="right hide-on-med-and-down">
---
+++ principal.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+
+    <body>
+        <?php
+        include 'head.php';
+        ?>
+        <?php
+        include 'menu.php';
+        ?>
+        
+        
+        <form action="val_NewMedicamento.php" method="post">
+            <div class="row contenedor">
+                <div class="card-panel  white conten">
+
+                    <div>
+                        <div class="input-field col s12">
+                            <input placeholder="ejm. Epamin" id="name" type="text" class="validate" name="nombre">
+                            <label for="first_name">Nombres del medicamento</label>
+                        </div>
+                        <div class="input-field col s12">
+                            <input placeholder="ejm. Epilepsias" id="last_names" type="text" class="validate" name="descripcion">
+                            <label for="last_name">Descripcion</label>
+                        </div>
+                        <div class="input-field col s12">
+                            <input placeholder="ejm. 23" id="user" type="text" class="validate" name="cantidad">
+                            <label for="user">Cantidad</label>
+                        </div>
+                    </div>
+                    <div class="center">
+                        <button class="waves-effect waves-light btn" type="submit">Guardar</button>
+                    </div>
+                </div>
+            </div>
+        </form>
+        <?php
+        include 'script.php';
+        ?>
+    </body>
+</html>
