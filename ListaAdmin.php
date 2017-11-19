2d08fe3af0937e6b25f4ff1e641e4e738f33fe15
Miguel Amaya <Miguel Amaya@DESKTOP-QVD2PQF>
2017-11-18 22:13:31 -0600

    ListaAdmin

--- ListaAdmin.php
+++ ListaAdmin.php
@@ -1,98 +1,91 @@
 <?php
-    session_start();
+session_start();
 ?>
 
 <!DOCTYPE html>
 <html lang="es">
-  
-  
-  <!-- **************** Incluir encabezados *************** --> 
 
-   
-   <?php
+
+    <!-- **************** Incluir encabezados *************** --> 
+
+
+    <?php
     include "cn.php";
-   ?>
-   
-   <!-- **************** Contenido *************** -->
+    ?>
+
+    <!-- **************** Contenido *************** -->
     <body>
-     
-     <?php
-            include "head.php";
-      ?>
-      
-      <!-- **************** Incluir Menu Alumno *************** -->
-       <?php
-            include "menu.php";
-       ?>
-       
-       
-       <div class="container white">
-           
+
+        <?php
+        include "head.php";
+        ?>
+
+        <!-- **************** Incluir Menu Alumno *************** -->
+        <?php
+        include "menu.php";
+        ?>
+
+
+        <div class="container white">
+
             <div class="container-fluid">
-             <h3 class="center-align">Lista Administradores</h3>
-            <br>
-                
-                 
+                <br>
+                <h3 class="center-align">Lista Administradores</h3>
+                <br>
+
+
                 <hr> 
-       
-       
 
-<form action="cliente_eliminar.php" method="post">
 
-            <table border="1" class="striped centered responsive-table">
-                <thead>
-                    <tr>
-                        <th>Nombres</th>
-                        <th>Apellidos</th>
-                        <th>Usuario</th>
-                        <th colspan="2">Acciones</th>
 
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
-                    $result = mysqli_query($conexion, "select * from administradores");
-                    while ($row = mysqli_fetch_assoc($result)) {
-                        ?>
-                        <tr>
-                            <td><?php echo $row['Usuario']; ?></td>
-                            <td><?php echo $row['Nombre']; ?></td>
-                            <td><?php echo $row['Email']; ?></td>
-                            <td><a class="tooltipped" data-position="bottom" data-delay="50" data-tooltip="Editar"  href="adminEditar.php?idAdministradores=<?php echo $row['idAdministradores']; ?>" ><i class="botoness2 zmdi zmdi-border-color zmdi-hc-2x "></i></a></td>
-                            <td><a class="tooltipped" data-position="bottom" data-delay="50" data-tooltip="Eliminar" href="#" onclick="javascript:if (confirm('QUIERES ELEIMINAR')) {
-                        parent.location = 'adminEliminar.php?idAdministradores=<?php echo $row['idAdministradores']; ?>'
-                    } else {
-                        ''
-                    }
-                    ;"><i class="botoness zmdi zmdi-delete zmdi-hc-2x mdc-text-red"></i></a></td>
-                            
-                            
-                            
-                        </tr>
+                <form action="cliente_eliminar.php" method="post">
+
+                    <table border="1" class="striped centered responsive-table">
+                        <thead>
+                            <tr>
+                                <th>Nombres</th>
+                                <th>Email</th>
+                                <th>Telefono Movil</th>
+                                <th colspan="1">Editar</th>
+
+                            </tr>
+                        </thead>
+                        <tbody>
                             <?php
-                        }
-                        mysqli_close($conexion);
-                        ?>
-                </tbody>
-            </table>
-        </form>
+                            $result = mysqli_query($conexion, "select * from administradores");
+                            while ($row = mysqli_fetch_assoc($result)) {
+                                ?>
+                                <tr>
+                                    <td><?php echo $row['Nombre']; ?></td>
+                                    <td><?php echo $row['Email']; ?></td>
+                                    <td><?php echo $row['TelMovil']; ?></td>
+                                    <td><a class="tooltipped" data-position="bottom" data-delay="50" data-tooltip="Editar"  href="AdminEditar.php?Usuario=<?php echo $row['Usuario']; ?>" ><i class=" zmdi zmdi-border-color zmdi-hc-2x "></i></a></td>
+                                </tr>
+                                <?php
+                            }
+                            mysqli_close($conexion);
+                            ?>
+                        </tbody>
+                    </table>
 
-           
-        
-           
-       </div>
-       </div>
-       
-       
-       
+                </form>
 
-       
-      
+
+
+
+            </div>
+        </div>
+
+
+
+
+
+
     </body>
-    
-     <!-- **************** Incluir script *************** -->
-       <?php
-            include "script.php";
-       ?>
-        
+
+    <!-- **************** Incluir script *************** -->
+    <?php
+    include "script.php";
+    ?>
+
 </html>
--- ListaUsuarios.php
+++ ListaUsuarios.php
@@ -34,14 +34,11 @@
             <table border="1" class="striped centered responsive-table">
                 <thead>
                     <tr>
-                        <th>N° de Usuario</th>
-                        <th>Usuario</th>
-                        <th>Password</th> 
+                        
                         <th>Nombre</th>
                         <th>N° Telefono</th>
                         <th>E-mail</th>
-                        <th>Administrador</th>
-                        <th colspan="2">Acciones</th>
+                        <th colspan="1">Acciones</th>
 
                     </tr>
                 </thead>
@@ -51,20 +48,16 @@
                     while ($row = mysqli_fetch_assoc($result)) {
                         ?>
                         <tr>
-                            <td><?php echo $row['idUsuario']; ?></td>
-                            <td><?php echo $row['Usuario']; ?></td>
-                            <td><?php echo $row['Password']; ?></td>
+                       
                             <td><?php echo $row['Nombre']; ?></td>
                             <td><?php echo $row['TelMovil']; ?></td>
                             <td><?php echo $row['Email']; ?></td>
-                            <td><?php echo $row['ADMINISTRADORES_idAdministradores']; ?></td>
-                            <td><a class="tooltipped" data-position="bottom" data-delay="50" data-tooltip="Editar"  href="adminEditar.php?idUsuario=<?php echo $row['idUsuario']; ?>" ><i class="botoness2 zmdi zmdi-border-color zmdi-hc-2x "></i></a></td>
-                            <td><a class="tooltipped" data-position="bottom" data-delay="50" data-tooltip="Eliminar" href="#" onclick="javascript:if (confirm('QUIERES ELIMINAR')) {
-                        parent.location = 'adminEliminar.php?idUsuario=<?php echo $row['idUsuario']; ?>'
+                            
+                        </tr>    
                     } else {
                         ''
                     }
-                    ;"><i class="botoness zmdi zmdi-delete zmdi-hc-2x mdc-text-red"></i></a></td>                                          
+                                                             
                         </tr>
                             <?php
                         }
